091271533fd5b09cde9e52fe3c23fae8a3bce982
matteo@gongon2.com <matteo@gongon2.com>
2020-09-29 08:26:42 +0200

    First commit

---
+++ data.js
@@ -0,0 +1,23 @@
+module.exports = {
+
+  token: '1075517277:AAEpUZp2m72f6KvdekwxOQ-MwHNmqjUugdY',
+  mongoLink: 'mongodb+srv://chat:admin@cluster0-g4hlr.mongodb.net/iktzi?retryWrites=true&w=majority',
+  chats: [-1001381307542, -1001166073886, -1001410544997], // your chats
+  botId: 1075517277, // bot id
+  devId: 62253745,
+
+  time: {
+    m: 60,
+    h: 3600,
+    d: 86400,
+    w: 604800
+  },
+  
+  timeNames: { // you can change lang.
+    m: 'мин.',
+    h: 'час.',
+    d: 'дн.',
+    w: 'нед.'
+  }
+
+}
